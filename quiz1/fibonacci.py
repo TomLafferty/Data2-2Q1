15f9ac4d16cd3bf24dee42d64842c7725c66d6a0
Thomas Lafferty <tsl5046@gmail.com>
2021-11-16 20:43:09 -0500

    fibonacci

--- quiz1/fibonacci.py
+++ quiz1/fibonacci.py
@@ -2,4 +2,26 @@ def fibonacci(fin: float) -> float:
     """
     build a classic fibonacci function - No RECURSION
     """
-    pass  # implement me
+    a = 0
+    b = 1
+    # For n < 0 not valid
+    if fin < 0:
+        print('Invalid input.')
+
+    # For n = 0 return 0
+    elif fin == 0:
+        return 0
+
+    # For n = 1 or 2 return 1
+    elif fin ==1:
+        return 1
+
+    # Else perform function
+    else:
+        for element in range(1, fin):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+    # pass  # implement me
