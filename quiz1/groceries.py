55fb7086280bbc13fc07eee1b52310b5417cbc8e
Thomas Lafferty <tsl5046@gmail.com>
2021-11-16 21:08:42 -0500

    done

--- quiz1/groceries.py
+++ quiz1/groceries.py
@@ -4,4 +4,18 @@ def checkout(cash: float, list: dict) -> float:
     from the cash passed into the function.
     return the "change" from the cash minus the total groceries value.
     """
-    pass  # implement me
+    # values = list.values()
+    total = sum(list.values())
+    change = cash - total
+    return change
+
+    # pass  # implement me
+# groc_dict = {
+#             'oatmeal': 4.59,
+#             'beefroast': 12.56,
+#             'cottagecheese': 3.99,
+#             'yogurt': 3.19,
+#             'nuts': 15.99,
+#             'bread': 4.99,
+#         }
+# checkout(100, groc_dict)
